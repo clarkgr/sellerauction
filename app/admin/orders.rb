666a371c9db027cc65ca456d180f9745381d6a83
Clark <giorgos.lamprianidis@gmail.com>
2012-07-23 23:15:30 +0300

    order for seller
    Signed-off-by: Clark <giorgos.lamprianidis@gmail.com>

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -34,23 +34,27 @@ ActiveAdmin.register Order do
           if resource.paid_at?
             I18n.l resource.paid_at
           else
-            raw <<-FORM
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-            <input type="hidden" name="cmd" value="_xclick">
-            <input type="hidden" name="business" value="F9HQ3DTTGYJQU">
-            <input type="hidden" name="lc" value="GR">
-            <input type="hidden" name="item_name" value="#{resource.interest.product.name}">
-            <input type="hidden" name="amount" value="#{resource.price}">
-            <input type="hidden" name="quantity" value="#{resource.interest.quantity}">
-            <input type="hidden" name="currency_code" value="EUR">
-            <input type="hidden" name="button_subtype" value="services">
-            <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted">
-            <input type="hidden" name="return" value="#{resource_url(resource, :story => "success")}">
-            <input type="hidden" name="cancel_return" value="#{resource_url(resource, :story => "cancel")}">
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-            </form>
-            FORM
+            if current_user.type == "Buyer"
+              raw <<-FORM
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+              <input type="hidden" name="cmd" value="_xclick">
+              <input type="hidden" name="business" value="F9HQ3DTTGYJQU">
+              <input type="hidden" name="lc" value="GR">
+              <input type="hidden" name="item_name" value="#{resource.interest.product.name}">
+              <input type="hidden" name="amount" value="#{resource.price}">
+              <input type="hidden" name="quantity" value="#{resource.interest.quantity}">
+              <input type="hidden" name="currency_code" value="EUR">
+              <input type="hidden" name="button_subtype" value="services">
+              <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHosted">
+              <input type="hidden" name="return" value="#{resource_url(resource, :story => "success")}">
+              <input type="hidden" name="cancel_return" value="#{resource_url(resource, :story => "cancel")}">
+              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+              <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+              </form>
+              FORM
+            else
+              "Buyer has not made a payment yet!"
+            end
           end
         end
         row :shipped_at
